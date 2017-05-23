d0f8af223450201862f165e6f3638077f2ead315
NMTechie <nileshranjan.mitra@yahoo.in>
2017-05-24 00:33:44 +0530

    The example of MultiContract Service

---
+++ MultipleWCFContracts/MultiContractClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MultiContractClient.MyServiceReference;
+
+namespace MultiContractClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client proxy1 = new Service1Client();
+            IService2 proxy2 = new Service2Client();
+
+            Console.WriteLine(proxy1.GetData(10));
+            Console.WriteLine(proxy2.DoWork("Nilesh"));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MultipleWCFContracts/ServiceHostApplication/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using MultipleWCFContracts;
+
+namespace ServiceHostApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ServiceHost host = null;
+            try
+            {
+                host = new ServiceHost(typeof(Service1));
+                BasicHttpBinding mybinding = new BasicHttpBinding(BasicHttpSecurityMode.None);
+                //host.AddServiceEndpoint(typeof(IService1), new BasicHttpBinding(), "http://localhost:8091/HostedService");
+                //host.AddServiceEndpoint(typeof(IService2), new BasicHttpBinding(), "http://localhost:8091/HostedService");
+                host.AddServiceEndpoint(typeof(IService1), mybinding, "http://localhost:8091/HostedService");
+                host.AddServiceEndpoint(typeof(IService2), mybinding, "http://localhost:8091/HostedService");
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                smb.HttpGetUrl = new Uri("http://localhost:8091/HostedService");
+                host.Description.Behaviors.Add(smb);
+                host.Open();
+                Console.WriteLine("The Service is Running");
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                if (host.State == CommunicationState.Faulted)
+                {
+                    host.Abort();
+                }
+            }
+            finally
+            {
+                host.Close();
+                Console.ReadLine();
+            }
+        }
+    }
+}
